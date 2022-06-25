943f1a6f6c43880ef897ec552ee53d9b584440df
Alex Fernandez <104288796+alex-fernandez-89@users.noreply.github.com>
2022-06-24 21:06:46 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+
+
+function copy() {alert("by ESTER FERRARI")}
+
+function fondoblack(){
+
+  document.body.style.backgroundColor="black";
+  document.getElementById("titulo1").style.color="white";
+  
+    }
+
+  function copy() {
+        alert("Autora:ESTER FERRARI. EMAIL: esterf24@gmail.com");
+        }
